210058099d43bab45f1929b16ba1515a636e2cf6
Felix Banguera <jorge.banguera@yuxiglobal.com>
2019-08-13 22:35:05 -0500

    FEAT added create contact form, and serach bar form. Used css grid layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  formdataCreate:FormGroup;
+  formdataSearch:FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.formdataCreate = new FormGroup({
+      firstName: new FormControl("", Validators.required),
+      lastName: new FormControl("", Validators.required),
+      phoneNumber: new FormControl("", Validators.required)
+   });
+
+    this.formdataSearch = new FormGroup({
+      searchInput: new FormControl("", Validators.required)
+    });
+  }
+
+  onClickSubmit(data){
+    console.log('onClickSubmit', data);
+  }
+
+  onClickSearch(data){
+    console.log('onClickSearch', data);
   }
 
 }
