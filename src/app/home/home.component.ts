63e8a8edf5c06b58c1d7f9cea5b5d6503275726d
Felix Banguera <jorge.banguera@yuxiglobal.com>
2019-08-14 22:31:03 -0500

    FEAT added search functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,11 +12,14 @@ export class HomeComponent implements OnInit {
   formdataCreate:FormGroup;
   formdataSearch:FormGroup;
   contacts:Array<Contact> = [];
+  contactsToShow:Array<Contact> = [];
+  searchCriteria:string="";
 
   constructor(private contactService: ContactsService) { }
 
   ngOnInit() {
     this.contacts = this.contactService.getContacts();
+    this.refreshContactsToShow();
     this.formdataCreate = new FormGroup({
       firstName: new FormControl("", Validators.required),
       lastName: new FormControl("", Validators.required),
@@ -37,6 +40,21 @@ export class HomeComponent implements OnInit {
 
   onClickSearch(data){
     console.log('onClickSearch', data);
+    this.searchCriteria = data.searchInput.trim();
+    this.refreshContactsToShow();
+  }
+
+  refreshContactsToShow(){
+    if(this.searchCriteria){
+      this.contactsToShow = this.contacts.filter(contact => {
+        console.log(this.searchCriteria, contact);
+        return (contact.firstName.toLowerCase().includes(this.searchCriteria) ||
+        contact.lastName.toLowerCase().includes(this.searchCriteria) ||
+        String(contact.phoneNumber).includes(this.searchCriteria))
+      })
+    }else{
+      this.contactsToShow = this.contacts;
+    }
   }
 
 }
